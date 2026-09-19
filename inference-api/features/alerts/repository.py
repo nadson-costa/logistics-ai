697c905bd48342c430a13afc783102a4632209c9
Nadson Costa <nadsonascosta@gmail.com>
2026-09-19 10:55:31 -0300

    feat(inference-api): recebe o pool por injeção e utiliza o poo.acquire()

--- inference-api/features/alerts/repository.py
+++ inference-api/features/alerts/repository.py
@@ -1,17 +1,16 @@
 import asyncpg
-from core.database import DB_URL
 from features.inspection.schemas import DetectionEvent
 
 class AlertRepository:
+    def __init__(self, pool: asyncpg.Pool):
+        self.pool = pool
+
     async def save_alert(self, event: DetectionEvent):
-        conn = await asyncpg.connect(DB_URL)
-        try:
+        async with self.pool.acquire() as conn:
             await conn.execute('''
                 INSERT INTO detection_alerts (event_type, frame_index, confidence, class_id)
                 VALUES ($1, $2, $3, $4)
             ''',
                 event.type, event.frame_index, event.confidence, event.class_id
             )
-            print(f"INFO: Salvando alerta no PostgreSQL: Frame {event.frame_index} | Confiança {event.confidence} | Classe {event.class_id}")
-        finally:
-            await conn.close()
+        print(f"INFO: Salvando alerta no PostgreSQL: Frame {event.frame_index} | Confiança {event.confidence} | Classe {event.class_id}")
